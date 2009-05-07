d5c9ce19b001ad7115798a40027b7fc9c792e3ed
Gerard Lledo <gerard.lledo@gmail.com>
2009-05-07 13:00:16 +0300

    Add basic logic operators (and fix equals)

--- ops/logic.s
+++ ops/logic.s
@@ -4,20 +4,36 @@
 	.include	"common.asi"
 	.include	"regdefs.asi"
 
-	@ All operators here take 2 parameters
-	.macro	GET_P p0, p1
+	@ All operators here take 2 parameters and do a compare
+	.macro	STK_CMP p0, p1
 		VPOP	\p0
 		VPOP	\p1
+		cmp	\p1, \p0
 	.endm
 
 	.text
 	.align	2
 	.global	op_equals	@ NAME: "="
+	.global op_greater_than	@ NAME: ">"
+	.global op_less_than	@ NAME: "<"
 
 op_equals:
-	GET_P	r0, r1
-	cmp	r0, r1
-	moveq	r0, #0		@ All bits CLEAR
-	movne	r0, #-1		@ All bits SET
+	STK_CMP	r0, r1
+	moveq	r0, #-1		@ All bits SET
+	movne	r0, #0		@ All bits CLEAR
+	VPUSH	r0
+	bx	lr
+
+op_greater_than:
+	STK_CMP	r0, r1
+	movgt	r0, #-1
+	movle	r0, #0
+	VPUSH	r0
+	bx	lr
+
+op_less_than:
+	STK_CMP	r0, r1
+	movlt	r0, #-1
+	movpl	r0, #0
 	VPUSH	r0
 	bx	lr
